e39f0614a29fa430e6f621b7b78b27fb505fe43e
Maggie Shazzy <maggieshazzy@gmail.com>
2025-08-07 13:03:29 +0300

    reviews

--- products/models.py
+++ products/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 from django.utils.text import slugify
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-
+from django.db.models import Avg
 
 def validate_image(file):
     valid_extensions = ['jpg', 'jpeg', 'png']
@@ -29,8 +29,6 @@ class Brand(models.Model):
 
     def __str__(self):
         return self.name
-
-from django.db.models import Avg
 
 class Product(models.Model):
     name = models.CharField(max_length=200)
